869b2b792257604d8839369113e38d9ac609c126
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-22 15:05:55 +0700

    range over channels

--- hello-world.go
+++ hello-world.go
@@ -1,34 +1,30 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	messages := make(chan string)
-	signals := make(chan bool)
+	jobs := make(chan int, 5)
+	done := make(chan bool)
 
-	select {
-	case msg := <-messages:
-		fmt.Println("received message", msg)
-	default:
-		fmt.Println("no messsage received")
-	}
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
 
-	msg := "hi"
-	select {
-	case messages <- msg:
-		fmt.Println("sent message", msg)
-	default:
-		fmt.Println("no messsage sent")
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
 	}
+	close(jobs)
+	fmt.Println("sent all jobs")
 
-	select {
-	case msg := <-messages:
-		fmt.Println("received message", msg)
-	case sig := <-signals:
-		fmt.Println("received signal", sig)
-	default:
-		fmt.Println("no activity")
-	}
+	<-done
 }
