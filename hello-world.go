36bc8994f6a6584437e6d7d5d0eb6dfb5a389452
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-22 14:32:41 +0700

    Channle directions

--- hello-world.go
+++ hello-world.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func worker(done chan bool) {
-	fmt.Println("working...")
-	time.Sleep(time.Second)
-	fmt.Println("done")
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
 
-	done <- true
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
 }
 
 func main() {
-	done := make(chan bool, 1)
-	go worker(done)
-
-	<-done
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	ping(pings, "passed message")
+	pong(pings, pongs)
+	fmt.Println(<-pongs)
 }
