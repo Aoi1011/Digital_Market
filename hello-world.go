3db8150ad518f3a6ef27921884ba5c31558bb622
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-22 10:32:10 +0700

    struct embedding

--- hello-world.go
+++ hello-world.go
@@ -2,48 +2,39 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-type geometry interface {
-	area() float64
-	perim() float64
+type base struct {
+	num int
 }
 
-type rect struct {
-	width, height float64
+func (b base) describe() string {
+	return fmt.Sprintf("base with num=%v", b.num)
 }
 
-type circle struct {
-	radius float64
-}
-
-func (r rect) area() float64 {
-	return r.width * r.height
-}
-
-func (r rect) perim() float64 {
-	return 2*r.width + 2*r.height
-}
-
-func (c circle) area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-
-func (c circle) perim() float64 {
-	return 2 * math.Pi * c.radius
-}
-
-func measure(g geometry) {
-	fmt.Println(g)
-	fmt.Println(g.area())
-	fmt.Println(g.perim())
+type container struct {
+	base
+	str string
 }
 
 func main() {
-	r := rect{width: 10, height: 5}
-	c := circle{radius: 5}
+	co := container{
+		base: base{
+			num: 1,
+		},
+		str: "some name",
+	}
 
-	measure(r)
-	measure(c)
+	fmt.Printf("co={num: %v, str: %v}\n", co.num, co.str)
+
+	fmt.Println("also num", co.base.num)
+
+	fmt.Println("describe:", co.describe())
+
+	type describer interface {
+		describe() string
+	}
+
+	var d describer = co
+	fmt.Println("describer:", d.describe())
 }
