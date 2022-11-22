fb99ee3ee7f136ca11dbdc704ef1d65387fd0cf1
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-22 11:41:47 +0700

    channels

--- hello-world.go
+++ hello-world.go
@@ -1,50 +1,14 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
 )
 
-func f1(arg int) (int, error) {
-	if arg == 42 {
-		return -1, errors.New("can't work with 42")
-	}
-
-	return arg + 3, nil
-}
-
-type argError struct {
-	arg  int
-	prob string
-}
-
-func (e *argError) Error() string {
-	return fmt.Sprintf("%d - %s", e.arg, e.prob)
-}
-
-func f2(arg int) (int, error) {
-	if arg == 42 {
-		return -1, &argError{arg, "can't work with it"}
-	}
-	return arg + 3, nil
-}
-
-func f(from string) {
-	for i := 0; i < 3; i++ {
-		fmt.Println(from, ":", i)
-	}
-}
-
 func main() {
-	f("direct")
+	messages := make(chan string)
 
-	go f("goroutine")
+	go func() { messages <- "ping" }()
 
-	go func(msg string) {
-		fmt.Println(msg)
-	}("going")
-
-	time.Sleep(time.Second)
-	fmt.Println("done")
+	msg := <-messages
+	fmt.Println(msg)
 }
