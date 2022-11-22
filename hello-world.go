3180b85cee23fe3e0c589c7f810d9ca3d1cfc973
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-22 11:21:29 +0700

    generics

--- hello-world.go
+++ hello-world.go
@@ -4,37 +4,51 @@ import (
 	"fmt"
 )
 
-type base struct {
-	num int
+func Mapkeys[K comparable, V any](m map[K]V) []K {
+	r := make([]K, 0, len(m))
+	for k := range m {
+		r = append(r, k)
+	}
+	return r
 }
 
-func (b base) describe() string {
-	return fmt.Sprintf("base with num=%v", b.num)
+type List[T any] struct {
+	head, tail *element[T]
 }
 
-type container struct {
-	base
-	str string
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+func (lst *List[T]) Push(v T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+
+func (lst *List[T]) GetAll() []T {
+	var elems []T
+	for e := lst.head; e != nil; e = e.next {
+		elems = append(elems, e.val)
+	}
+	return elems
 }
 
 func main() {
-	co := container{
-		base: base{
-			num: 1,
-		},
-		str: "some name",
-	}
+	var m = map[int]string{1: "2", 2: "4", 3: "8"}
 
-	fmt.Printf("co={num: %v, str: %v}\n", co.num, co.str)
+	fmt.Println("keys:", Mapkeys(m))
 
-	fmt.Println("also num", co.base.num)
+	_ = Mapkeys(m)
 
-	fmt.Println("describe:", co.describe())
-
-	type describer interface {
-		describe() string
-	}
-
-	var d describer = co
-	fmt.Println("describer:", d.describe())
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(13)
+	lst.Push(23)
+	fmt.Println("list:", lst.GetAll())
 }
