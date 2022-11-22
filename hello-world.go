2416056b21836269a4001840b735815f3b0c2ffe
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-22 07:07:05 +0700

    slices

--- hello-world.go
+++ hello-world.go
@@ -5,24 +5,47 @@ import (
 )
 
 func main() {
-	var a [5]int
-	fmt.Println("emp:", a)
 
-	a[4] = 100
-	fmt.Println("set:", a)
-	fmt.Println("get:", a[4])
+	s := make([]string, 3)
+	fmt.Println("emp", s)
 
-	fmt.Println("len:", len(a))
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
 
-	b := [5]int{1, 2, 3, 4, 5}
-	fmt.Println("dcl:", b)
+	fmt.Println("len:", len(s))
 
-	var twoD [2][3]int
-	for i := 0; i < 2; i++ {
-		for j := 0; j < 3; j++ {
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd:", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy:", c)
+
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl2:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innnerLen := i + 1
+		twoD[i] = make([]int, innnerLen)
+		for j := 0; j < innnerLen; j++ {
 			twoD[i][j] = i + j
 		}
 	}
 
 	fmt.Println("2d: ", twoD)
+
 }
