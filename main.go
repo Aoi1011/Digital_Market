39ec28c916625f4c8aba148eccbde0d46341bfd3
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-23 14:14:05 +0700

    The http.Handler Type

--- main.go
+++ main.go
@@ -5,14 +5,16 @@ import (
 	"net/http"
 )
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
 	default:
-		// TODO: handle the page not found error
+		http.Error(w, "Page not found", http.StatusNotFound)
 	}
 }
 
@@ -27,7 +29,7 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
+	var router Router
 	fmt.Printf("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
