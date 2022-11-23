5736645033408deec32d45758994ead9eb10f30c
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-23 13:56:02 +0700

    Switch

--- main.go
+++ main.go
@@ -5,6 +5,17 @@ import (
 	"net/http"
 )
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// TODO: handle the page not found error
+	}
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
@@ -15,13 +26,8 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"a@gmail.com\">a@gmail.com</a></p>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, r.URL.Path)
-}
-
 func main() {
 	http.HandleFunc("/", pathHandler)
-	http.HandleFunc("/contact", contactHandler)
 	fmt.Printf("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
