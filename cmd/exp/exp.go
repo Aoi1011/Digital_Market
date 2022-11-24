f4562afc178779e9cf5f23a624b293388e823cc5
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-24 22:07:22 +0700

    Install pgx

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,58 +1,24 @@
 package main
 
 import (
-	"errors"
+	"database/sql"
 	"fmt"
-	"strings"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-func Connect() error {
-	return errors.New("connection failed")
-}
-
-func CreateUser() error {
-	err := Connect()
-	if err != nil {
-		return fmt.Errorf("create user: %w", err)
-	}
-	return nil
-}
-
-func CreateOrg() error {
-	err := CreateUser()
-	if err != nil {
-		return fmt.Errorf("create org: %w", err)
-	}
-	return nil
-}
-
-func Demo(numbers ...int) {
-	for _, number := range numbers {
-		fmt.Print(number, " ")
-	}
-	fmt.Println()
-}
-
-func Sum(numbers ...int) int {
-	sum := 0
-	for i := 0; i < len(numbers); i++ {
-		sum += numbers[i]
-	}
-	return sum
-}
-
-func Join(vals ...string) string {
-	var sb strings.Builder
-	for i, s := range vals {
-		sb.WriteString(s)
-		if i < len(vals)-1 {
-			sb.WriteString(", ")
-		}
-	}
-	return sb.String()
-}
-
 func main() {
-	strings := []string{"the", "quick", "brown", "fox"}
-	fmt.Println(Join(strings...))
+	db, err := sql.Open("pgx", "host=localhost port=5432 user=baloo password=junglebook dbname=lenslocked sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected!")
+
+	defer db.Close()
+
 }
