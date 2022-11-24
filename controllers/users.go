f7487bf26c5836fdbcf440f1f839213a76f59a3f
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-24 16:53:47 +0700

    Handle signup post

--- controllers/users.go
+++ controllers/users.go
@@ -16,5 +16,11 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Temporary response")
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Unable to parse from submission", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "<p>Email: %s</p>", r.PostForm.Get("email"))
+	fmt.Fprintf(w, "<p>Password: %s</p>", r.PostForm.Get("password"))
 }
